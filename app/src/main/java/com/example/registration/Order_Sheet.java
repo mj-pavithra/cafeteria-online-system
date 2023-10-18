8979accdd9ba9eefd713ebabd945bfe2efefbc79
MJ Pavithra <mjpavithra6520@gmail.com>
2023-10-18 20:20:19 +0530

    registation is done

---
+++ app/src/main/java/com/example/registration/Order_Sheet.java
@@ -0,0 +1,14 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Order_Sheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_sheet);
+    }
+}
